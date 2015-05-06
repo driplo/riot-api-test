e3ea3d3fdd46faa106a7e4386eda6419f0240a3b
AdrienAuer <aauer@silamir.com>
2015-05-06 16:42:22 +0200

    Added weekly champions

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,41 @@
-app.controller('MainController', ['$scope', 'champions', function($scope, champions) { 
+API_KEY = 'f1fe37d2-6955-48ce-bc41-7cbc084efd83';
+
+app.controller('MainController', ['$scope', 'champions', 'freechampions', '$http',  function($scope, champions, freechampions, $http) { 
  champions.success(function(data) { 
-    $scope.champions = data; 
+    $scope.champions = data;
   });
-
-
+    
+    
+    freeChamps = [];
+    freechampions.success(function(data, status, headers, config) {
+		$scope.freechampions = data;
+        console.log('Nombre de champions free:'+$scope.freechampions.champions.length);
+        for (i=0; i < $scope.freechampions.champions.length; i++){
+            $http({method: 'GET', url: 'https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/' + $scope.freechampions.champions[i].id+'?champData=image' + '&api_key=' + API_KEY }).
+			success(function(data2, status2, headers2, config2) {
+				// success
+				freeChamps.push(data2);
+                $scope.freeChamps = freeChamps;
+			});
+        }
+    });
+    
+    
+    
 }]);
+
+
+/*
+    success(function(data, status, headers, config) {
+		// success
+		for (i = 0; i < data.champions.length; ++i) {
+			$http({method: 'GET', url: 'https://eune.api.pvp.net/api/lol/static-data/eune/v1.2/champion/' + data.champions[i].id + '?api_key=' + API_KEY }).
+			success(function(data2, status2, headers2, config2) {
+				// success
+				freeChamps.push(data2);
+			}).
+			error(function(data2, status2, headers2, config2) {
+				// TODO error
+			});
+		}
+    })*/
